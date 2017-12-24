aa80ff86547ea984e99608473893b31474a9fff8
Your Name <you@example.com>
2017-12-23 22:25:17 -0800

    test.php

--- test/test.php
+++ test/test.php
@@ -157,3 +157,14 @@ printf("cost time:" . ($endTs-$startTs)*1000 . "ms" . PHP_EOL);
 $dataArr = json_decode($dataRaw,true);
 
 print_r($dataArr); 
+
+//TEST 13
+$startTs = microtime();
+
+$dataRaw = file_get_contents("http://127.0.0.1:9003/?query_word=科技&query_num");
+
+$endTs = microtime();
+
+printf("cost time:" . ($endTs-$startTs)*1000 . "ms" . PHP_EOL);
+
+print_r($dataRaw);
